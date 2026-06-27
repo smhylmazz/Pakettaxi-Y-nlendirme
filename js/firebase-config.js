995dc4ce3ca3f0f06c831b1e1146c0dafc44f825
smhylmazz <ysemih619@gmail.com>
2026-06-27 16:41:45 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,19 @@
-// Firebase yapılandırması buraya gelecek
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+
+// BURAYA FIREBASE CONSOLE'DAN GELEN firebaseConfig GELECEK
+const firebaseConfig = {
+  // apiKey: "AIzaSyA3If6Dw02u86lERztg3P05GLfac1C0jLI",
+    authDomain: "yonlendirme-81a45.firebaseapp.com",
+    projectId: "yonlendirme-81a45",
+    storageBucket: "yonlendirme-81a45.firebasestorage.app",
+    messagingSenderId: "126920950281",
+    appId: "1:126920950281:web:0dc2f5e4fa8018dc4f6ed8"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
